4bce7995ba741e0b17344bb34eda014795272b2b
LailaWulandarii <E41220418@student.polije.ac.id>
2025-03-01 10:14:56 +0700

    fix typo on username-user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'usename',
+        'username',
         'password',
         'role',
         'priority',
